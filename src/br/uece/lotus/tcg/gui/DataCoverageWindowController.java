952e1e596ce0c24977a3b4b75d5393c5a2a11664
gabrielRodrigues <gabriel.rodrigues.uece@gmail.com>
2016-05-05 11:53:20 -0300

    added Data Coverage initial configurations

--- src/br/uece/lotus/tcg/gui/DataCoverageWindowController.java
+++ src/br/uece/lotus/tcg/gui/DataCoverageWindowController.java
@@ -6,6 +6,7 @@
 package br.uece.lotus.tcg.gui;
 
 import br.uece.lotus.Component;
+import br.uece.lotus.Transition;
 import br.uece.lotus.tcg.struct.LtsInfo;
 import br.uece.lotus.tcg.utils.DebugLog;
 import br.uece.lotus.viewer.ComponentViewImpl;
@@ -22,13 +23,14 @@ import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+
 /**
  *
  * @author Gabriel
  */
-public class DataCoverageWindowController implements Initializable{
-    
-     @FXML
+public class DataCoverageWindowController implements Initializable {
+
+    @FXML
     private ScrollPane mScrollPane;
 
     @FXML
@@ -50,18 +52,54 @@ public class DataCoverageWindowController implements Initializable{
     private TabPane mTabPane;
 
     @FXML
-    private ComboBox<?> mGenCombo;
+    private ComboBox<String> mGenCombo;
 
     @FXML
     private HBox mHboxGen;
-    
+
     protected ComponentViewImpl mViewer;
-    
+
     protected LtsInfo mLtsInfo;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        DebugLog.printLog("DataCoverageWindowController -> initialize", ">> Start");
+
+        mViewer = new ComponentViewImpl();
+        mScrollPane.setContent(mViewer);
+
+        Component component = (Component) resources.getObject("component");
+        mViewer.setComponent(component);
+
+        mLtsInfo = new LtsInfo(component.getInitialState());
+        mButtonGenHelp = new Button();
+
+        initGuardInfo();
+    }
+
+    protected void initGuardInfo() {
+
+        Iterable<Transition> trasitionsList = mViewer.getComponent().getTransitions();
+
+        for (Transition transition : trasitionsList) {
+            if (transition.getGuard() != null) {
+                mGenCombo.getItems().add(transition.getGuard());
+            }
+
+        }
+        if (mGenCombo.getItems().size() > 0) {
+            mGenCombo.setValue(mGenCombo.getItems().get(0));
+        }
+    }
+
+    protected String getSelectedGuard() {
+        return mGenCombo.getSelectionModel().getSelectedItem();
+    }
 
     @FXML
     void onClickGeneratorCombobox(ActionEvent event) {
-
+        String guard = getSelectedGuard();
+        System.err.println("Guard Selected: "+ guard);
     }
 
     @FXML
@@ -71,22 +109,10 @@ public class DataCoverageWindowController implements Initializable{
 
     @FXML
     void onSubmit(ActionEvent event) {
-
+         String value = mTextFieldGenParam.getText();
+         System.err.println("Test Value: "+ value);
+         
+         //DataCOverageClass
+         
     }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        DebugLog.printLog("DataCoverageWindowController -> initialize", ">> Start");
-
-        mViewer = new ComponentViewImpl();
-        mScrollPane.setContent(mViewer);
-        
-        Component component = (Component) resources.getObject("component");
-        mViewer.setComponent(component);
-
-        mLtsInfo = new LtsInfo(component.getInitialState());
-
-        
-    }
-    
 }
